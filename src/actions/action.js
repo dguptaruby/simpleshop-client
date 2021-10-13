cf894cdd27a7dbf916a2387f802ca6c9841c3ddc
developer <chhatrapal882>
2021-10-13 16:00:03 +0530

    changes in action.js file

--- src/actions/action.js
+++ src/actions/action.js
@@ -3,26 +3,25 @@ import axios from "axios";
 const token = localStorage.getItem('token')
 export const auth_login = (data) => {
     return (dispatch) => {
-  
-          if (token === null) {
-            localStorage.setItem("token","");
-            token = "";
-    
-          }
-          else {
-            axios.get(`${process.env.REACT_APP_LINK}/auto_login`,
-              { headers: { "Authorization": `Bearer ${token}` } })
-              .then(res => {
-                if (res?.data) {
-                  return dispatch(checklogin(res.data))
-                }
-              })
-              .catch(err => console.log(err))
-          }
+
+        if (token === null) {
+            localStorage.setItem("token", "");
+
         }
+        else {
+            axios.get(`${process.env.REACT_APP_LINK}/auto_login`,
+                { headers: { "Authorization": `Bearer ${token}` } })
+                .then(res => {
+                    if (res?.data) {
+                        return dispatch(checklogin(res.data))
+                    }
+                })
+                .catch(err => console.log(err))
+        }
+    }
 }
 
-export const checklogin = (user) =>{
+export const checklogin = (user) => {
     console.log('fffffffffff');
     console.log('came here1')
 
